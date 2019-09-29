4fb0485ce2826ee4f8c0d4c8f8fe355718283b62
Михаил <optimisist@gmail.com>
2019-09-29 21:54:21 +0300

    Updates

---
+++ precache-manifest.02a686234390843a612921e1ad4478ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7695bd50923a5693e4ec0554508f73d",
+    "url": "/afisha/index.html"
+  },
+  {
+    "revision": "d47644c4f68705d5803c",
+    "url": "/afisha/static/css/2.cc82b09a.chunk.css"
+  },
+  {
+    "revision": "8e04756ef86eca861c13",
+    "url": "/afisha/static/css/main.df9f4119.chunk.css"
+  },
+  {
+    "revision": "d47644c4f68705d5803c",
+    "url": "/afisha/static/js/2.31228afd.chunk.js"
+  },
+  {
+    "revision": "8e04756ef86eca861c13",
+    "url": "/afisha/static/js/main.0fde1248.chunk.js"
+  },
+  {
+    "revision": "1590a7a709fee17b4c6b",
+    "url": "/afisha/static/js/runtime-main.a24db899.js"
+  }
+]);
